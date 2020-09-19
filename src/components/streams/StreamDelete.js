d561364e5ee4f33ae418d43744bd84a503bd030a
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-09-19 10:39:27 +0530

    commit

---
+++ src/components/streams/StreamDelete.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Modal from "../Modal";
+import history from "../../history";
+import { fetchStream, deleteStream } from "../../actions";
+
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  renderContent() {
+    if (!this.props.stream) {
+      return "are You sure ?";
+    }
+    return `Are you sure you want to Delete "${this.props.stream.title}"`;
+  }
+
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button
+          onClick={() => this.props.deleteStream(this.props.match.params.id)}
+          className="ui button negative"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <Modal
+        onDismiss={() => history.push("/")}
+        title="Delete Stream"
+        content={this.renderContent()}
+        actions={this.renderActions()}
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
---
+++ src/components/streams/StreamForm.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+
+class StreamForm extends React.Component {
+  renderError(meta) {
+    if (meta.touched && meta.error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{meta.error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = (formProps) => {
+    //console.log(formProps);
+    const className = `field ${
+      formProps.meta.error && formProps.meta.touched ? "error" : ""
+    }`;
+    return (
+      <div className={className}>
+        <label>{formProps.label}</label>
+        <input {...formProps.input} autoComplete="off" />
+        {this.renderError(formProps.meta)}
+      </div>
+    );
+  };
+
+  onSubmit = (formValues) => {
+    this.props.onSubmit(formValues);
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form error"
+      >
+        <Field label="ENTER TITLE" name="title" component={this.renderInput} />
+        <Field
+          label="ENTER DESCRIPTION"
+          name="description"
+          component={this.renderInput}
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "INVALID TITLE";
+  }
+  if (!formValues.description) {
+    errors.description = "INVALID INPUT";
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: "streamForm",
+  validate: validate,
+})(StreamForm);
